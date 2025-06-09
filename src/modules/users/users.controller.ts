cbb454d1e6e24503bdbfa44cc704f22487843bc0
Rody Huancas <rodyhuancas.04@gmail.com>
2025-06-08 19:17:10 -0500

    Feat: Replace IdValidationPipe with UUIDValidationPipe in UsersController

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,7 +3,7 @@ import { Auth } from '../auth/decorators/auth.decorator';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { IdValidationPipe } from 'src/common/pipes/id-validation.pipe';
+import { UUIDValidationPipe } from 'src/common/pipes/uuid-validation.pipe';
 
 @Controller('users')
 export class UsersController {
@@ -21,17 +21,17 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id', IdValidationPipe) id: string) {
+  findOne(@Param('id', UUIDValidationPipe) id: string) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id', IdValidationPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(@Param('id', UUIDValidationPipe) id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', IdValidationPipe) id: string) {
+  remove(@Param('id', UUIDValidationPipe) id: string) {
     return this.usersService.remove(id);
   }
 }
