b9c7a37c0fb4794d835680647702d6cd93858c05
Rody Huancas <rodyhuancas.04@gmail.com>
2025-06-17 21:00:54 -0500

    Feat: Update PermissionsController and PermissionsService to use UUID for permission IDs

--- src/modules/permissions/permissions.controller.ts
+++ src/modules/permissions/permissions.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { UUIDValidationPipe } from 'src/common/pipes/uuid-validation.pipe';
 
 @Controller('permissions')
 export class PermissionsController {
@@ -18,17 +19,17 @@ export class PermissionsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.permissionsService.findOne(+id);
+  findOne(@Param('id', UUIDValidationPipe) id: string) {
+    return this.permissionsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
-    return this.permissionsService.update(+id, updatePermissionDto);
+  update(@Param('id', UUIDValidationPipe) id: string, @Body() updatePermissionDto: UpdatePermissionDto) {
+    return this.permissionsService.update(id, updatePermissionDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.permissionsService.remove(+id);
+  remove(@Param('id', UUIDValidationPipe) id: string) {
+    return this.permissionsService.remove(id);
   }
 }
